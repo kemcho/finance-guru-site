2e38adc0bea4e5ea62b90250fe3dc602900f0a91
Kunal Cholera <kcholera@kcholera-mn3.linkedin.biz>
2018-11-23 14:11:42 -0800

    new user registration working

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,52 @@
 const express = require('express');
-
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcryptjs = require('bcryptjs');
 
+
+// @route   GET api/users/test
+// @desc    test route to see if everything is working fine
+// @access  public
 router.get('/test', (req,res) => res.json({msg:'Users works'}));
+
+const User = require('../../models/User');
+
+// @route   POST api/users/post
+// @desc    register a new user or throw an error if user exists
+// @access  public
+router.post('/register', (req,res) => {
+    User.findOne({email: req.body.email})
+        .then( user => {
+            if(user) {
+                return res.status(400).json({email: "User Already exists"});
+            } else {
+                //get the avatar of the user
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200',
+                    r: 'pg',
+                    d: 'mm'
+                });
+                //create the new user
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                });
+
+                bcryptjs.genSalt(10,(err, salt) => {
+                    bcryptjs.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    });
+                });
+
+            };
+        });
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,8 +6,16 @@ const express = require('express');
 
 //instanciate the app and start it to listen on a port
 const app = express();
+
+// use body parser : Todo: find out why body parser three lines below does not work when you put it after the other app.use lines
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log (`Server running on port ${port}`));
+
 
 //connect to our mongodb database via mongoose
 const db = require("./config/keys").mongoURI;
@@ -21,15 +29,11 @@ mongoose.connect(db,{useNewUrlParser:true})
 app.get('/', (req, res) => res.send("Hello World Kunal"));
 
 
+
 //import all the resources
 const users = require('./routes/api/users');
 app.use('/api/users', users);
-const profiles = require('./routes/api/profiles')
+const profiles = require('./routes/api/profiles');
 app.use('/api/profiles', profiles);
-const posts = require('./routes/api/posts')
+const posts = require('./routes/api/posts');
 app.use('/api/posts', posts);
-
-
-
-
-
