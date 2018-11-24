6598edfb1db11a8149874a1cf1d77dc0e2aee37a
Kunal Cholera <kcholera@kcholera-mn3.linkedin.biz>
2018-11-23 16:13:32 -0800

    new login endpoint to create a json-wek-token for valid users

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = require('../../config/keys');
 
 
 // @route   GET api/users/test
@@ -46,6 +48,38 @@ router.post('/register', (req,res) => {
 
             };
         });
+});
+
+
+// @route   POST api/user/login
+// @desc    checks if the user enters correct password and returns a JWT token
+// @access  public
+router.post("/login",(req,res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email}).then( user => {
+        if(!user) return res.status(404).json({email: 'User not found'});
+
+        //check plain text password with hashed password
+        bcryptjs.compare(password, user.password).then( isMatch => {
+            if(isMatch){
+                //send down the signed jason web token back to the client
+                const payload = {id: user.id, name: user.name, avatar: user.gravatar};
+
+                jwt.sign(payload, secret.mySecretKey, {expiresIn: 3600}, (err, token) => {
+                    res.json({
+                        success: true,
+                        token: 'Bearer ' + token
+                    });
+                });
+                
+            } else {
+                return res.status(400).json({password: "invalid password"});
+            }
+
+        });
+    });
 });
 
 
