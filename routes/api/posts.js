725676c5979e3db7fa36c82aae0b6c99ff41e584
Kunal Cholera <kcholera@kcholera-mn3.linkedin.biz>
2018-12-15 15:56:42 -0800

    add ability to add and remove comments from a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -66,7 +66,7 @@ router.post('/', passport.authenticate('jwt', {session:false}), (req,res) => {
 
 });
 
-// @route   /api/users/posts/like/:id
+// @route   /api/posts/like/:id
 // @desc    like a post
 // @access  private
 router.post('/like/:id', passport.authenticate('jwt', {session:false}), (req, res)=>{
@@ -85,7 +85,7 @@ router.post('/like/:id', passport.authenticate('jwt', {session:false}), (req, re
 });
 
 
-// @route   /api/users/posts/unlike/:id
+// @route   /api/posts/unlike/:id
 // @desc    unlike a post
 // @access  private
 router.post('/unlike/:id', passport.authenticate('jwt', {session:false}), (req, res)=>{
@@ -104,6 +104,60 @@ router.post('/unlike/:id', passport.authenticate('jwt', {session:false}), (req, 
         }).catch(err => res.status(404).json({postNotFound: "error with unliking, post not found"}));
 
 });
+
+// @route   /api/posts/comment/:id
+// @desc    add a comment to a post
+// @access  private
+router.post('/comment/:id', passport.authenticate('jwt', {session:false}), (req, res)=>{
+
+    Post.findById(req.params.id)
+        .then(post => {
+            
+            //use the same validation for comment as that of the post
+            //todo: use custom comment validator, to not have confusing error messages, here we will see same error message for post and comment
+            const {errors,isValid} = ValidatePostInput(req.body);
+            if(!isValid){
+                return res.status(400).json(errors);
+            }
+
+            const newComment = {
+                user: req.user.id,
+                text: req.body.text,
+                name: req.body.name,
+                avatar:req.body.avatar
+            };
+
+            post.comments.unshift(newComment);
+            post.save().then(post => res.json(post));
+
+        }).catch(err => res.status(404).json(err.toString()));
+
+});
+
+// @route   /api/posts/comment/:post_id/:comment_id
+// @desc    remove a comment from a post
+// @access  private
+router.delete('/comment/:post_id/:comment_id', passport.authenticate('jwt', {session:false}), (req, res)=>{
+
+    Post.findById(req.params.post_id)
+        .then(post => {
+        
+            //check if it valid comment id that belongs to the user if so remove it
+            if(post.comments
+                .filter(comment => 
+                        comment._id.toString() === req.params.comment_id &&
+                        comment.user.toString() === req.user.id.toString()).length === 0){
+                    return res.status(404).json({commentDoesNotExists: "comment does not exist for this user"});
+                }
+            //get the index of the comment the user owns and remove it
+            const removeIndex = post.comments.map(item => item._id.toString()).indexOf(req.params.comment_id.toString());
+            post.comments.splice(removeIndex,1);
+            post.save().then(post => res.json(post));
+
+        }).catch(err => res.status(404).json(err.toString()));
+
+});
+
 
 
 
