2ae3d61d5f1c9013b96dce45300e21d07afca7ed
Kunal Cholera <kcholera@kcholera-mn3.linkedin.biz>
2018-12-15 22:05:38 -0800

    add react client code with footer, nav bar and landing pages

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+
+
+class App extends Component {
+  render() {
+    return (
+      
+      <div className="App">
+        <NavBar/>
+        <Landing/>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
