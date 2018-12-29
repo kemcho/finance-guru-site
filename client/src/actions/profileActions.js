093df55ca488c9a760acd3e3e172c75df20df06c
Kunal Cholera <kcholera@kcholera-mn3.linkedin.biz>
2018-12-28 19:07:53 -0800

    add code to create profile from UI form

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE} from '../actions/types';
+import {GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS} from '../actions/types';
 
 //Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -22,3 +22,16 @@ export const clearCurrentProfile = () => {
         type: CLEAR_CURRENT_PROFILE
     }
 }
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+    axios
+      .post('/api/profiles', profileData)
+      .then(res => history.push('/dashboard'))
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  };
