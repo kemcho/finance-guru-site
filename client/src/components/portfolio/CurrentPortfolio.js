35c10f2d5591109684ebe8dd8bf0e017456d2f19
raxitcholera <raxitcholera@gmail.com>
2019-02-14 20:24:24 -0800

    Initial Table display for current portfolio

--- client/src/components/portfolio/CurrentPortfolio.js
+++ client/src/components/portfolio/CurrentPortfolio.js
@@ -1,53 +1,80 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {getAllTransactions} from '../../actions/portfolioActions';
-import PropTypes from 'prop-types';
-import isEmpty from '../../validations/is-empty'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getAllTransactions } from "../../actions/portfolioActions";
+import PropTypes from "prop-types";
+import isEmpty from "../../validations/is-empty";
+import { Table } from "antd";
 
 class CurrentPortfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displaySocialInputs: false,
+      errors: {},
+      portfolio: []
+    };
+  }
+
   //call action to get all transactions for a users portfolio
-  componentDidMount(){
+  componentDidMount() {
     this.props.getAllTransactions();
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.portfolio) {
-        this.setState({ portfolio: nextProps.portfolio });
+      this.setState({ portfolio: nextProps.portfolio });
     }
   }
- 
-  render() {  
 
-    const {portfolio} = this.props.portfolio;
-    let content;
-
-    if(isEmpty(portfolio)){
-        content = "Loading your current portfolio ...";
-    }else{
-        content = JSON.stringify(portfolio);
-    }
+  render() {
+    const columns = [
+      {
+        title: "Ticker Name",
+        dataIndex: "Stock Ticker",
+        key: "Stock Ticker"
+      },
+      {
+        title: "Units",
+        dataIndex: "Units",
+        key: "Units"
+      },
+      {
+        title: "Current Price",
+        dataIndex: "Current Price",
+        key: "Current Price"
+      }
+    ];
 
     return (
       <div>
         <h1>Portfolio</h1>
-        {content}
+        <Table
+          size="small"
+          rowKey="Stock Ticker"
+          bordered={true}
+          pagination={false}
+          columns={columns}
+          dataSource={this.state.portfolio.portfolio}
+        />
       </div>
-    )
+    );
   }
 }
 
-
 CurrentPortfolio.propTypes = {
-    getAllTransactions: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    portfolio: PropTypes.object.isRequired
-  }
-  
-  const mapStateToProps = state => ({
-    auth: state.auth,
-    portfolio: state.portfolio
-  });
+  getAllTransactions: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  portfolio: PropTypes.object.isRequired
+};
 
-export default connect(mapStateToProps, {getAllTransactions})(CurrentPortfolio);
+const mapStateToProps = state => ({
+  auth: state.auth,
+  portfolio: state.portfolio
+});
+
+export default connect(
+  mapStateToProps,
+  { getAllTransactions }
+)(CurrentPortfolio);
 
 //Todo: Add Test+ Add error handling when backend server fails to respond with data (unknown symbol for e.g.)
