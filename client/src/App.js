9f2360d8c4261d6a37333aa71b669d8bfc7dfd3a
Raxit Cholera <raxitcholera@gmail.com>
2019-07-21 16:23:07 -0700

    Add New Transactions (#10)
    * Add ability to add new stock transactions to portfolio
    
    This will resolve #5 and #6 bugs as well.

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,45 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {Provider} from 'react-redux';
-import store from './store';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
-import jwt_decode from 'jwt-decode';
-import PrivateRoute from './components/common/PrivateRoute';
-import CreateProfile from './components/create-profile/CreateProfile';
-import CurrentPortfolio from './components/portfolio/CurrentPortfolio';
-import AddExperience from './components/add-credentials/AddExperience';
-import AddEducation from './components/add-credentials/AddEducation';
-import Dashboard2 from './components/dashboard/Dashboard2';
+import jwt_decode from "jwt-decode";
+import PrivateRoute from "./components/common/PrivateRoute";
+import CreateProfile from "./components/create-profile/CreateProfile";
+import CurrentPortfolio from "./components/portfolio/CurrentPortfolio";
+import AddExperience from "./components/add-credentials/AddExperience";
+import AddTransaction from "./components/portfolio/AddTransaction";
+import AddEducation from "./components/add-credentials/AddEducation";
+import Dashboard2 from "./components/dashboard/Dashboard2";
 
-import './App.css';
-import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser, logoutUser } from './actions/authActions';
-import { clearCurrentProfile } from './actions/profileActions';
-import EditProfile from './components/edit-profile/EditProfile';
+import "./App.css";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
+import EditProfile from "./components/edit-profile/EditProfile";
 
 //check for login token and ensure user stays logged in
-if(localStorage.jwtToken){
+if (localStorage.jwtToken) {
   //Set the auth token header for this current page
   setAuthToken(localStorage.jwtToken);
   //Set the current user as well from the token in store
   const decoded = jwt_decode(localStorage.jwtToken);
   store.dispatch(setCurrentUser(decoded));
-  
+
   //check if expired token
-  const currentTimeInSeconds = Date.now()/1000;
-  if(decoded.exp < currentTimeInSeconds){
+  const currentTimeInSeconds = Date.now() / 1000;
+  if (decoded.exp < currentTimeInSeconds) {
     store.dispatch(logoutUser());
     store.dispatch(clearCurrentProfile());
-    window.location.href = './login';
+    window.location.href = "./login";
   }
-
-
 }
-
 
 class App extends Component {
   render() {
@@ -50,38 +48,62 @@ class App extends Component {
         <Router>
           <div className="App">
             <Navbar />
-            <Route exact path="/" component={Landing}/>
+            <Route exact path="/" component={Landing} />
             <div className="container">
-              <Route exact path="/register" component={Register}/>
-              <Route exact path="/login" component={Login}/>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
               <Switch>
-                <PrivateRoute exact path="/dashboard" component={Dashboard2}/>
+                <PrivateRoute exact path="/dashboard" component={Dashboard2} />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/current-portfolio" component={CurrentPortfolio}/>
+                <PrivateRoute
+                  exact
+                  path="/current-portfolio"
+                  component={CurrentPortfolio}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/add-experience" component={AddExperience} />
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/add-education" component={AddEducation} />
+                <PrivateRoute
+                  exact
+                  path="/add-transactions"
+                  component={AddTransaction}
+                />
               </Switch>
-            </div>          
-            <Footer/>        
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
+                />
+              </Switch>
+            </div>
+            <Footer />
           </div>
         </Router>
-
       </Provider>
     );
   }
 }
 
 export default App;
-
-
