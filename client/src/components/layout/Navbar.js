7ff3978e559064bbddcdea48ff23961420a54f46
Kunal Cholera <usckmc@gmail.com>
2019-07-21 15:05:29 -0700

    add a link to stock-portfolio in navigation, incorporate formating changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {logoutUser} from '../../actions/authActions';
-import {clearCurrentProfile} from '../../actions/profileActions';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 
 class Navbar extends Component {
   onLogoutClick(e) {
@@ -11,43 +11,54 @@ class Navbar extends Component {
     this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
-  
+
   render() {
-    const {isAuthenticated, user} = this.props.auth;
-    
+    const { isAuthenticated, user } = this.props.auth;
+
     const authLinks = (
       <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/dashboard">
-              Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <a href="/logout" onClick={this.onLogoutClick.bind(this)}
-            className="nav-link">
-              <img src={user.avatar} alt={user.name} style={{width: '25px', marginRight: '5px'}}
-               title="you must have a gravatar connected to see the image"></img>
-               {' '} Logout
-            </a>     
-          </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/current-portfolio">
+            Stock Portfolio
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
+          <a
+            href="/logout"
+            onClick={this.onLogoutClick.bind(this)}
+            className="nav-link"
+          >
+            <img
+              src={user.avatar}
+              alt={user.name}
+              style={{ width: "25px", marginRight: "5px" }}
+              title="you must have a gravatar connected to see the image"
+            />{" "}
+            Logout
+          </a>
+        </li>
       </ul>
     );
 
     const guestLinks = (
       <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-                <Link className="nav-link" to="/register">
-                  Sign Up
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">
+            Sign Up
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
       </ul>
     );
-
 
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
@@ -68,14 +79,13 @@ class Navbar extends Component {
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
                 <a className="nav-link" href="profiles.html">
-                  {' '}
+                  {" "}
                   Developers
                 </a>
               </li>
             </ul>
 
-            {isAuthenticated? authLinks: guestLinks}
-
+            {isAuthenticated ? authLinks : guestLinks}
           </div>
         </div>
       </nav>
@@ -86,9 +96,12 @@ class Navbar extends Component {
 Navbar.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
-}
-const mapStateToProps = (state) => ({
+};
+const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, {logoutUser, clearCurrentProfile})(Navbar);
+export default connect(
+  mapStateToProps,
+  { logoutUser, clearCurrentProfile }
+)(Navbar);
